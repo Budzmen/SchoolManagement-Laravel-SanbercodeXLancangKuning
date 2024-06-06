f06446428cf36ec0e48c80ff714db6574815eed4
Marigel03 <kigentzy@gmail.com>
2024-06-06 19:52:46 +0700

    Menambahkan file migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -12,11 +12,11 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->id()->notnull();
+            $table->string('name',45);
+            $table->string('email',45)->unique();
+            $table->string('password',45);
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
             $table->rememberToken();
             $table->timestamps();
         });
---
+++ database/migrations/2024_06_06_104759_create_subjects_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSubjectsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('subjects', function (Blueprint $table) {
+            $table->id(); // Ini akan membuat kolom id sebagai bigint dengan auto_increment dan primary key
+            $table->string('name', 50); // Nama mata pelajaran dengan panjang maksimum 50 karakter
+            $table->unsignedBigInteger('teacher_id'); // Foreign key untuk teachers
+            $table->unsignedBigInteger('student_id'); // Foreign key untuk students
+            $table->timestamps(); // Kolom created_at dan updated_at
+
+            // Definisi foreign key
+            $table->foreign('teacher_id')->references('id')->on('teachers')->onDelete('cascade');
+            $table->foreign('student_id')->references('id')->on('students')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('subjects');
+    }
+}
